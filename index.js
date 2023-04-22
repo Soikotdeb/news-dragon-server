d82657650e9e5e9a56ded45f75910a1b36b7049f
soikotdeb <sdsoikot424@gmail.com>
2023-04-22 14:04:02 +0600

    send data in cliend server

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const Catagorys = require('./Data/Catagorys.json')
+const news = require('./Data/News.json');
+
 
 
 app.use(cors());
@@ -17,6 +19,34 @@ res.send('dragon is running ')
 app.get('/categories',(req, res)=>{
     res.send(Catagorys);
 
+})
+
+// All news
+app.get('/news', (req,res )=>{
+    res.send(news)
+})
+
+// specific id news
+app.get('/news/:id', (req, res)=>{
+    const id= req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n=>n._id === id);
+    res.send(selectedNews)
+})
+
+
+// category news
+app.get('/categories/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=>parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+   
 })
 
 app.listen(port,()=>{
